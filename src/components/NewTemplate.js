563b4f0cd2b797071c788177affa8cedef0e0811
chino0rt24 <ortegapablo0224@gmail.com>
2023-05-23 10:28:13 -0600

    creacion de componentes

---
+++ src/components/NewTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NewTemplate() {
+  return (
+   
+    <div>
+      <h2>SideMenu Component</h2>
+     
+    </div>
+  );
+}
+
+export default NewTemplate;
---
+++ src/components/SideMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SideMenu() {
+  return (
+    
+    <div>
+      <h2>SideMenu Component</h2>
+     
+    </div>
+  );
+}
+
+export default SideMenu;
