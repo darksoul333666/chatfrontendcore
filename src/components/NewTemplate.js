4fd0f3b24205fe75765e254a84bd52f2e2637555
chino0rt24 <ortegapablo0224@gmail.com>
2023-05-24 14:39:44 -0600

    vista newtmplate

--- src/components/NewTemplate.js
+++ src/components/NewTemplate.js
@@ -1,13 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Button from '@mui/material/Button';
 
-function NewTemplate() {
+const NewTemplate = () => {
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [trainingMethod, setTrainingMethod] = useState('');
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Lógica para generar la respuesta utilizando la biblioteca openai o cualquier otra opción
+
+    // Ejemplo de generación de respuesta utilizando una función dummy
+    const generatedResponse = await generateResponse(prompt);
+    setResponse(generatedResponse);
+  };
+
+  const generateResponse = async (prompt) => {
+    // Lógica para generar la respuesta utilizando la biblioteca openai o cualquier otra opción
+    // Puedes usar una API, una biblioteca de cliente o cualquier otro método
+
+    // Ejemplo de función dummy que simplemente devuelve el prompt invertido
+    const reversedPrompt = prompt.split('').reverse().join('');
+    return reversedPrompt;
+  };
+
+  const handleTrainingMethodChange = (event, newTrainingMethod) => {
+    setTrainingMethod(newTrainingMethod);
+  };
+
+  const handleFileUpload = (event) => {
+    const uploadedFile = event.target.files[0];
+    setFile(uploadedFile);
+  };
+
+  const handleSave = () => {
+    // Lógica para guardar los datos, incluyendo el archivo
+    console.log('Saving data:', {
+      prompt,
+      response,
+      trainingMethod,
+      file
+    });
+  };
+
   return (
-   
     <div>
-      <h2>SideMenu Component</h2>
-     
+      <React.Fragment>
+        <CssBaseline />
+        <Container fixed>
+          <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+            <h1 style={{ textAlign: 'center' }}>New Template</h1>
+            <div style={{ textAlign: 'center' }}>
+              <label htmlFor="NameTemplet">Name of the new template:</label>
+              <TextField id="outlined-basic" label="write the name" variant="outlined" />
+              <h3 style={{ textAlign: 'center' }}>Please Select Your training method</h3>
+              <ToggleButtonGroup
+                orientation="vertical"
+                value={trainingMethod}
+                exclusive
+                onChange={handleTrainingMethodChange}
+                sx={{ mt: 2 }}
+              >
+                <ToggleButton value="method1" sx={{ width: '100%' }}>
+                TRAIN WITH TEXT  <input type="file" onChange={handleFileUpload} />
+                </ToggleButton>
+                <ToggleButton value="method2" sx={{ width: '100%' }}>
+                TRAIN WITH FILE  <input type="file" onChange={handleFileUpload} />
+                </ToggleButton>
+                <ToggleButton value="method3" sx={{ width: '100%' }}>
+                TRAIN WITH WEBSITE <input type="file" onChange={handleFileUpload} />
+                </ToggleButton>
+              </ToggleButtonGroup>
+              <h3 style={{ textAlign: 'center' }}>Now click on save to finish</h3>
+       
+              <Button variant="contained" disableElevation 
+            style={{ textAlign: 'center' }}>
+      Save
+    </Button>
+            </div>
+           
+          </Box>
+        </Container>
+      </React.Fragment>
     </div>
   );
-}
+};
 
 export default NewTemplate;
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
+import { makeStyles, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import DescriptionIcon from '@material-ui/icons/Description';
+import AddIcon from '@material-ui/icons/Add';
+const useStyles = makeStyles((theme) => ({
+    drawer: {
+      width: '240px',
+      backgroundColor:'#BCDBD6',
+    },
+    listItem: {
+      paddingLeft: theme.spacing(2),
+    },
+  }));
 
 function SideMenu() {
+    const classes = useStyles();
   return (
     
     <div style={{width:"100%", height:"100%", backgroundColor:"red", display:"flex", flexDirection:"row" }} >
-    <div style={{width:"30%", height:"100%", backgroundColor:"blue" }} >
+    <div style={{width:"15%", height:"100%", backgroundColor:"blue" }} >
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawer,
+      }}
+      
+    >
+       
+        
+      <List>
+        <ListItem button className={classes.listItem}>
+          <ListItemIcon>
+            <ChatIcon />
+          </ListItemIcon>
+          <ListItemText primary="New Chat" />
+        </ListItem>
+        <ListItem button className={classes.listItem}>
+          <ListItemIcon>
+            <DescriptionIcon />
+          </ListItemIcon>
+          <ListItemText primary="Templates" />
+        </ListItem>
+        <ListItem button className={classes.listItem}>
+          <ListItemIcon>
+            <AddIcon />
+          </ListItemIcon>
+          <ListItemText primary="Add New" />
+        </ListItem>
+      </List>
+     
+    </Drawer>
           </div>
-    <div style={{width:"70%", height:"100%",  backgroundColor:"green"}} >
+    <div style={{width:"85%", height:"100%",  backgroundColor:"white"}} >
+        <Outlet></Outlet>
     </div>
     </div>
   );
