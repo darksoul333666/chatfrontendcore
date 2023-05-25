bbc7740f0901dff1fd4b7926d71807ae6324f169
chino0rt24 <ortegapablo0224@gmail.com>
2023-05-25 14:13:10 -0600

    cambio maqueta y chat

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,24 +1,32 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { MessageList } from 'react-chat-elements';
 import 'react-chat-elements/dist/main.css';
 import HeaderComponent from '../components/HeaderTemplate';
 import { API, ROUTES } from '../api';
+import 'react-chat-elements/dist/main.css';
 
 const ChatComponent = () => {
   const [messages, setMessages] = useState([]);
   const messageListReference = useRef(null);
-
-  
-  
- 
-
-  const handleMessageSubmit =async (event) => {
+  const [isTyping, setIsTyping] = useState(false);
+  const handleMessageSubmit = async (event) => {
     event.preventDefault();
     const userInput = event.target.message.value;
-    //const response = await API.post(ROUTES.GET_AI_RESPONS, JSON.stringify({ input:userInput}));
-    const response = await (await API()).post(ROUTES.GET_AI_RESPONS, JSON.stringify({ input:userInput}));
-    const userMessage = { position: 'right', type: 'text', text: userInput, date: new Date() };
-    const aiMessage = { position: 'left', type: 'text', text: response.data.data, date: new Date() };
+    const response = await (await API()).post(ROUTES.GET_AI_RESPONS, JSON.stringify({ input: userInput }));
+    const userMessage = {
+      position: 'right',
+      type: 'text',
+      text: userInput,
+      date: new Date(),
+      style: { color: 'blue' },
+    };
+    const aiMessage = {
+      position: 'left',
+      type: 'text',
+      text: response.data.data,
+      date: new Date(),
+      style: { color: 'red' },
+    };
 
     setMessages([...messages, userMessage, aiMessage]);
 
@@ -28,30 +36,38 @@ const ChatComponent = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <HeaderComponent />
-      <MessageList
-        ref={messageListReference}
-        className="message-list"
-        lockable={true}
-        toBottomHeight={'100%'}
-        dataSource={messages.map((message, index) => ({
-          ...message,
-          id: index,
-        }))}
-        styles={{
-          message: {
-            right: {
-              '.triangle::after': {
-                display: 'none',
+      <div style={{ height: 'calc(100vh - 100px)', overflowY: 'auto' }}>
+        <MessageList
+          ref={messageListReference}
+          className="message-list"
+          lockable={true}
+          toBottomHeight={'100%'}
+          dataSource={messages.map((message, index) => ({
+            ...message,
+            id: index,
+            position: message.position === 'right' ? 'right' : 'left',
+            style: { background: message.position === 'right' ? 'blue' : 'red' },
+          }))}
+          styles={{
+            message: {
+              right: {
+                '.triangle::after': {
+                  display: 'none',
+                  background:'red',
+                },
+              },
+              left: {
+                '.triangle::after': {
+                  display: 'none',
+                  background:'blue',
+                },
               },
             },
-            left: {
-              '.triangle::after': {
-                display: 'none',
-              },
-            },
-          },
-        }}
-      />
+          }}
+        />
+         {isTyping && <div>El usuario está escribiendo...</div>}
+      </div>
+      <div style={{flex:1, flexDirection: 'column', height: '100vh', alignItems: 'center', justifyContent: 'center',marginLeft:350 }} >
       <form onSubmit={handleMessageSubmit} className="message-input">
         <div
           style={{
@@ -62,7 +78,7 @@ const ChatComponent = () => {
             padding: '4px 8px',
             margin: '8px 0',
             position: 'fixed',
-            width: 399,
+            width: '399px',
             bottom: 0,
           }}
         >
@@ -93,6 +109,7 @@ const ChatComponent = () => {
           </button>
         </div>
       </form>
+      </div>
     </div>
   );
 };
--- src/components/HeaderTemplate.js
+++ src/components/HeaderTemplate.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import  Avatar  from '@mui/material/Avatar';
-import TypingText from './TypiygText';
-const HeaderComponent = () => (
-    <Box sx={{ flexGrow: 1, backgroundColor: "red" }}>
-  <AppBar position="static">
-    <Toolbar sx={{ justifyContent: "center", flex:1, display:'flex' }}>
-    <h1>Hello! Welcome to your Chat Bot. How can I assist you today?</h1>
-        <Avatar sx={{ bgcolor: "green", width:75, height:75 }}></Avatar>
-        {/* <Button color="inherit">Asistente</Button> */}
-      {/* <Button color="inherit">Right Button</Button> */}
-    </Toolbar>
-  </AppBar>
-</Box>
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
-  );
+const HeaderComponent = () => (
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static" sx={{ borderRadius: '0 0 10px 10px' }}>
+      <Toolbar sx={{ justifyContent: 'center' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
+          <Typography variant="h6" component="h1" sx={{ mr: 2, flexGrow: 1 }}>
+            Hello! Welcome to your Chat Bot. How can I assist you today?
+          </Typography>
+          <Avatar sx={{ bgcolor: 'green', width: 75, height: 75 }} />
+        </Box>
+        {/* <Button color="inherit">Asistente</Button> */}
+        {/* <Button color="inherit">Right Button</Button> */}
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
 
 export default HeaderComponent;
+
